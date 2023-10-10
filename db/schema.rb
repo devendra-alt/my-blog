899c23007ca60fd70bc88f80b16e0b97a7012047
devendra-alt <devendramulewa171@gmail.com>
2023-10-10 22:25:09 +0530

    Add likes in schema

--- db/schema.rb
+++ db/schema.rb
@@ -6,4 +6,11 @@
     t.bigint "post_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
