84d604b37d8ca726892316160261a14a484b892e
devendra-alt <devendramulewa171@gmail.com>
2023-10-10 22:24:52 +0530

    Add comments in schema

---
+++ db/schema.rb
@@ -0,0 +1,9 @@
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
