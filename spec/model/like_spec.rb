911591f6af974ef054097790d616f07c57ddc342
devendra-alt <devendramulewa171@gmail.com>
2023-10-13 00:21:08 +0530

    Add likes counter check on non integer and negative values

--- spec/model/like_spec.rb
+++ spec/model/like_spec.rb
@@ -16,4 +16,12 @@ RSpec.describe Like, type: :model do
   it 'user like count should be 1' do
     expect(like.user.likes.size).to be(1)
   end
+  it 'should be invalid on negetive like counter' do
+    like.post.likes_count = -1
+    expect(like.post).to_not be_valid
+  end
+  it 'should not be valid for non integer like counter' do
+    like.post.likes_count = 'dd'
+    expect(like.post).to_not be_valid
+  end
 end
