9279c4d72fcbee492d5778e3e2b48f39216f9799
devendra-alt <devendramulewa171@gmail.com>
2023-10-20 11:08:39 +0530

    Add test for Post #index and #show methods

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  subject(:user) { User.create(name: 'mike', photo: 'https//link.photo-me', bio: 'I am a software developer') }
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'should give response as 200' do
+      get "/users/#{user.id}/posts"
+      expect(response).to have_http_status(200)
+    end
+    it 'should render #index template' do
+      get "/users/#{user.id}/posts"
+      expect(response).to render_template(:index)
+    end
+    it 'should render text on template' do
+      get "/users/#{user.id}/posts"
+      response_body = response.body
+      expect(response_body).to include('Posts')
+    end
+  end
+  describe 'GET /show' do
+    subject(:post) { Post.create(title: 'my-blog-post', text: 'this is my blog post', author_id: user.id) }
+    it 'should give response as 200' do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to have_http_status(200)
+    end
+    it 'should render #index template' do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to render_template(:show)
+    end
+    it 'should render text on template' do
+      get "/users/#{user.id}/posts/#{post.id}"
+      response_body = response.body
+      expect(response_body).to include('hello posts')
+    end
   end
 end
