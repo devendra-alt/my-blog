d4dbfc755b4f55aed4b90f9132eb9fe6f83ea3b5
devendra-alt <devendramulewa171@gmail.com>
2023-10-21 19:19:39 +0530

    Add new post and new comment routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,14 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show'
   get '/users/:user_id/posts', to: 'posts#index'
   get '/users/:user_id/posts/:id', to: 'posts#show'
+  get '/posts/:post_id/comments/new', to: 'comments#new'
+  get '/users/:user_id/posts/new', to: 'posts#new'
 
   get '/home', to: 'users#index'
   root 'users#index'
+
+  post '/users/:user_id/posts/create', to: 'posts#create'
+  post '/users/:user_id/posts/:post_id/like_comment', to: 'likes#create'
+  post '/users/:user_id/posts/:post_id/create_comment', to: 'comments#create'
   # Defines the root path route ("/")
 end
