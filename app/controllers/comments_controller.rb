fe8f235a147877f30d113cb5a0e95a4a54a6614a
devendra-alt <devendramulewa171@gmail.com>
2023-10-21 23:56:38 +0530

    Added required changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,13 @@
 class CommentsController < ApplicationController
-  def new
-    @comment = Comment.new
-    @post = Post.find(params[:post_id])
-    @current_user = set_current_user
-  end
-
   def create
-    @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
+    @user = User.find(params[:user_id])
     @comment = Comment.new(comment_params)
+
     if @comment.save
       redirect_to "/users/#{@user.id}/posts/#{@post.id}"
     else
-      flash[:error] = 'ERROR! while posting comment'
+      flash[:error] = 'There was an error posting the comment, please try again.'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
+  get '/users/:user_id', to: 'users#show'
   get '/users/:user_id/posts', to: 'posts#index'
-  get '/users/:user_id/posts/:id', to: 'posts#show'
-  get '/posts/:post_id/comments/new', to: 'comments#new'
   get '/users/:user_id/posts/new', to: 'posts#new'
-
-  get '/home', to: 'users#index'
-  root 'users#index'
+  get '/users/:user_id/posts/:post_id', to: 'posts#show'
 
   post '/users/:user_id/posts/create', to: 'posts#create'
   post '/users/:user_id/posts/:post_id/like_comment', to: 'likes#create'
   post '/users/:user_id/posts/:post_id/create_comment', to: 'comments#create'
+
+  root "users#index"
   # Defines the root path route ("/")
 end
