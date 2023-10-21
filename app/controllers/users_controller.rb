fb812dd7db62ce6e3a75736123ece9d7ef1d949b
devendra-alt <devendramulewa171@gmail.com>
2023-10-21 10:52:48 +0530

    Add show and index action to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
-  def show; end
-  def index; end
+  def show
+    @user = User.find(params[:id])
+    @posts = Post.where(author_id: params[:id]).order(created_at: :desc)
+  end
+
+  def index
+    @user = User.order(created_at: :asc)
+    @user.each do |user|
+      puts user
+    end
+  end
 end
